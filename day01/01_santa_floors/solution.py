a01c6b10bc2a269c3316015b11e6b834fac8d851
JPinSPACE <jp@jpinspace.com>
2015-12-02 17:06:59 -0500

    Update day 1 to pass pylint

--- day01/01_santa_floors/solution.py
+++ day01/01_santa_floors/solution.py
@@ -1,19 +1,26 @@
-# Solution to the first puzzle of Day 1 on adventofcode.com
+""" Solution to the first puzzle of Day 1 on adventofcode.com
+"""
 
-floor = 0
-with open('santa_input', 'r') as input_file:
-    while True:
-        char = input_file.read(1)
-        if not char:
-            break
+def main():
+    """ Calculate what floor Santa is on.
+    """
+    floor = 0
+    with open('santa_input', 'r') as input_file:
+        while True:
+            char = input_file.read(1)
+            if not char:
+                break
 
-        if char == '(':
-            floor += 1
-        elif char == ')':
-            floor -= 1
-        elif char == '\n':
-            continue
-        else:
-            print "Unexpected character: '{}'".format(char)
+            if char == '(':
+                floor += 1
+            elif char == ')':
+                floor -= 1
+            elif char == '\n':
+                continue
+            else:
+                print "Unexpected character: '{}'".format(char)
 
-print "Santa is on floor: {}".format(floor)
+    print "Santa is on floor: {}".format(floor)
+
+if __name__ == '__main__':
+    main()
--- day01/02_santa_basement_adventure/solution.py
+++ day01/02_santa_basement_adventure/solution.py
@@ -1,23 +1,31 @@
-# Solution to the first puzzle of Day 1 on adventofcode.com
+""" Solution to the second puzzle of Day 1 on adventofcode.com
+"""
 
-floor = 0
-char_pos = 0
-with open('santa_input', 'r') as input_file:
-    while True:
-        char_pos += 1
-        char = input_file.read(1)
-        if not char:
-            break
+def main():
+    """ Determine when Santa entered the basement
+    """
+    floor = 0
+    char_pos = 0
+    with open('santa_input', 'r') as input_file:
+        while True:
+            char_pos += 1
+            char = input_file.read(1)
+            if not char:
+                break
 
-        if char == '(':
-            floor += 1
-        elif char == ')':
-            floor -= 1
-        elif char == '\n':
-            continue
-        else:
-            print "Unexpected character: '{}'".format(char)
+            if char == '(':
+                floor += 1
+            elif char == ')':
+                floor -= 1
+            elif char == '\n':
+                continue
+            else:
+                print "Unexpected character: '{}'".format(char)
 
-        if floor < 0:
-            print "Santa entered the basement at character position: '{}'".format(char_pos)
-            break;
+            if floor < 0:
+                print "Santa entered the basement at " \
+                      "character position: '{}'".format(char_pos)
+                break
+
+if __name__ == '__main__':
+    main()
